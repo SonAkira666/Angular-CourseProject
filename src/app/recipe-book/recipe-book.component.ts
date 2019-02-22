1c0008e0599ad030706c18174946eaba30c8f4b3
Son_Akira <Son_Akira666@hotmail.com>
2019-02-22 11:28:52 +0100

    Prima avevo aggiunto anche tante altre cose, adesso solo che cambia il dettaglio selezionando la recipe

--- src/app/recipe-book/recipe-book.component.ts
+++ src/app/recipe-book/recipe-book.component.ts
@@ -6,5 +6,10 @@ import { Recipe } from "./recipe";
 	templateUrl: "./recipe-book.component.html"
 })
 export class RecipeBookComponent {
-	recipe: Recipe[] = [new Recipe('Parmigiana' , 'Na bella porcata.' , 'https://www.ricettedellanonna.net/wp-content/uploads/2016/05/ricetta-parmigiana-alle-melanzane.jpg') , new Recipe('Parmigiana' , 'Na bella porcata.' , 'https://www.ricettedellanonna.net/wp-content/uploads/2016/05/ricetta-parmigiana-alle-melanzane.jpg')];
+	recipe: Recipe[] = [new Recipe('Parmigiana' , 'Na bella porcata.' , 'https://www.ricettedellanonna.net/wp-content/uploads/2016/05/ricetta-parmigiana-alle-melanzane.jpg') , new Recipe('Parmigiana Meglia' , 'Na bellissima porcata.' , 'https://www.ricettedellanonna.net/wp-content/uploads/2016/05/ricetta-parmigiana-alle-melanzane.jpg')];
+	activeRecipe: Recipe = new Recipe("Recipe Name", "No description", "");
+
+	setActiveRecipe(r: Recipe) {
+		this.activeRecipe = r;
+	}
 }
